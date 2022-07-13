9c254bb27f90583df7b54f20c1e7f7fd531f2d7a
Mehul-Gupta-2177 <mehul.gupta@tufts.edu>
2022-07-13 14:33:14 -0400

    Add

--- my.py
+++ my.py
@@ -42,7 +42,8 @@ def averageClaimJuly2017():
 def stats():
     file = "rx_demo.xlsx"
     data = pd.read_excel(file) #reading file
-    column = data["Plan_Pay"]
+    data_top_5 = data.loc[data['Plan_Name'].isin(['UAW RETIREE MEDICAL BENEFITS TRUST','CASH TX','HCSC BCBS TX','HUMANA HEALTH PLAN PDP32','CIGNA-HEALTHSPRING TX'])]
+    column = data_top_5["Plan_Pay"]
     max_value = column.max()
     min_value = column.min()
     min_value2 = column.drop_duplicates().nsmallest(2).iloc[-1]
@@ -57,8 +58,29 @@ def stats():
     #lowest excluding 0
     print(avg_val)
     
-        
-        
+def top5insuranceClaimProviders():
+
+    file = "rx_demo.xlsx"
+    df = pd.read_excel (file)
+    df_2016 = df.loc[df['monthYear'].isin(['2016-01','2016-02','2016-03','2016-04','2016-05','2016-06','2016-07','2016-08','2016-09','2016-10','2016-11','2016-12'])]
+    #print(df_2016)
+    fig = px.bar(df_2016, x='Plan_Name')
+    fig.show()
+    
+    
+def TotalAmountPaid():
+    file = "rx_demo.xlsx"
+    df = pd.read_excel (file)
+
+    df_top_5 = df.loc[df['Plan_Name'].isin(['UAW RETIREE MEDICAL BENEFITS TRUST','CASH TX','HCSC BCBS TX','HUMANA HEALTH PLAN PDP32','CIGNA-HEALTHSPRING TX'])]
+    #print(df_top_5)
+    #fig = px.bar(df_top_5, x='Plan_Name')
+    #fig.show()
+    df_top5_2017 = df_top_5.loc[df_top_5['monthYear'].isin(['2017-01','2017-02','2017-03','2017-04','2017-05','2017-06','2017-07','2017-08','2017-09','2017-10','2017-11','2017-12'])]
+    #print(df_top5_2017)
+    fig = px.bar(df_top5_2017, x="Plan_Name", y=["Plan_Pay", "Secondary_Plan_Pay"])
+    fig.show()
+    
     
    
         
